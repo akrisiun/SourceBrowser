7b81d428fc71833563658bb0d9891e8f432fde8d
AndriusK <akrisiun@gmail.com>
2017-09-15 17:54:41 +0300

    RuntimeIdentifiers +osx.10 for net46

--- dotnet-build.ps1
+++ dotnet-build.ps1
@@ -1,5 +1,15 @@
 
-dotnet build src\HtmlGenerator\HtmlGenerator.csproj -o ..\..\bin
+$os = [Environment]::OSVersion.VersionString
+
+if ($os.StartsWith("Unix"))
+{
+    # dotnet : 2.0.0-preview1-005977/Microsoft.Common.CurrentVersion.targets(1111,5): 
+    # error MSB3644: The reference assemblies for framework ".NETFramework,Version=v4.6"
+    msbuild /v:m src\HtmlGenerator\HtmlGenerator.csproj
+}
+else { 
+    dotnet build src\HtmlGenerator\HtmlGenerator.csproj -o ..\..\bin
+}
 
 dotnet build src\SourceIndexServer\SourceIndexServer.csproj  -o ..\..\web
 
