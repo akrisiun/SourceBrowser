8e7ee3b0d8f20c30ac9fd3d7814da0ef5586fe4c
AndriusK <akrisiun@gmail.com>
2017-09-15 11:13:30 +0300

    index.html for :5000 port only :(

--- src/SourceIndexServer/web.ps1
+++ src/SourceIndexServer/web.ps1
@@ -1,16 +1,22 @@
-#powershell
-#!/bin/bash
+# powershell web.ps1
+# ps | grep Server.dll
 
-$Env:FOLDER = "/Users/andriusk/Sites/System.Web3"
+$Env:FOLDER = "/Users/andriusk/Sites/System.Web3/Index"
 Write-Host "FOLDER=$Env:FOLDER"
-
 # [Environment]::GetEnvironmentVariable("PATH","User")
-if (![System.IO.File]::Exists("SourceIndexServer.dll")) {
-    Write-Host "$Env:PWD bin/SourceIndexServer.dll"
+
+$dll = "$pwd/SourceIndexServer.dll"
+# Write-Host $dll
+
+if (![System.IO.File]::Exists($dll)) {
+    Write-Host "$Env:PWD/bin/Debug/netcoreapp1.1/SourceIndexServer.dll"
     dotnet bin/Debug/netcoreapp1.1/SourceIndexServer.dll
 }
 else {  
-    Write-Host "Server: $Env:PWD SourceIndexServer.dll"
-    dotnet SourceIndexServer.dll  --urls "http://*:5001"
+    Write-Host "Server: $pwd SourceIndexServer.dll"
+    Write-Host "http://localhost:5001/index.html#q="
+    $urls = "http://*:5000"
+    # $global:p += Start-Process dotnet -ArgumentList "SourceIndexServer.dll" -PassThru
+    Start-Process dotnet -ArgumentList "SourceIndexServer.dll --urls $urls" -PassThru
 }
 
